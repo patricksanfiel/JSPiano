27ba37ef9c9428dfaa6850b8e3577c545b4a63dd
Patrick Sanfiel <patricksanfiel@gmail.com>
2019-01-28 19:50:03 -0500

    'play' class is added to the div corresponding to a users keypress. After a transitionend event the play class is removed from the div

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,17 @@
 window.onload = (event) => {
-    console.log("ready")
-    document.addEventListener("keydown", playNote)
     function playNote(event){
         const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)//Grab an audio element with a "data-key" attribute that matches the keycode of the key pressed by the user.
+        const keyPressed = document.querySelector(`div[data-key="${event.keyCode}"]`)//Grab the key pressed by the user
         if(!audio) return//If there is no audio element that corresponds to the key pressed by the user, the value of audio will be null and we will return without executing the rest of the function.
+        keyPressed.classList.add("play")
+        audio.currentTime = 0;//Reset current time to 0 on every keypress so users will be able to hit the same note in rapid succession without having to wait for the audio to finish playing before hearing it again.
         audio.play()
     }
+    function removePlayClass(event){
+        this.classList.remove("play")
+    }
+    document.addEventListener("keydown", playNote)
+    let allKeys = document.querySelectorAll(".piano-key")
+    allKeys.forEach(key=>key.addEventListener("transitionend", removePlayClass))
+    console.log(allKeys)
 }
